96b6404b113b69fe1b051b0b32bb094cd6c10243
Asher Seiling <f14asher@gmail.com>
2021-03-07 18:06:36 -0600

    Added file imprting options

---
+++ main.py
@@ -0,0 +1,22 @@
+import regular_expression
+
+
+# The data for the file storage
+file_adress = ""
+
+# Arrays
+encoded_text = []
+abreviation_array = []
+reference_array = []
+
+def compress():
+    pass
+
+# Class for importing files to compress
+class import_files:
+    def import_code():
+        print("What is the adress of the file that you want to compress")
+        file_adress = input()
+    def import_compresed_file():
+        print("Place the adress of the file that you want to decompress")
+        file_adress = input()
